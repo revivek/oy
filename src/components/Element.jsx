dc261b8cc7dda4b7c4cbc7a93ab0a7d2004f5abe
Vivek Patel <vivek@oysterbooks.com>
2016-02-16 22:43:56 -0500

    Remove support for Oy.Element type prop. Fixes #30

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -6,25 +6,17 @@ import OyProps from './OyProps';
 
 const Element = (props) => {
   return React.createElement(
-    props.tagName || props.type,
+    props.tagName,
     objectAssign(
       {},
       OyProps.convertToTransformableProps(props),
-      {type: null, tagName: null}
+      {tagName: null}
     ),
     props.children
   );
 };
 
 Element.propTypes = {
-  type: (_, propName) => {
-    if (propName === 'type') {
-      new Error(
-        'The `type` prop on Oy.Element is deprecated' +
-        ' and will be removed in 0.4.0. Use `tagName` instead.'
-      )
-    }
-  },
   tagName: React.PropTypes.string.isRequired
 };
 
