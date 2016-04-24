7fea9c261706a6f8c5a8a3f2e178b93b4266ea9f
Vivek Patel <vivek@oysterbooks.com>
2016-04-24 12:25:49 -0400

    Allow mailto and tel in href attributes. Fixes #47

--- src/rules/HrefAbsoluteURLRule.js
+++ src/rules/HrefAbsoluteURLRule.js
@@ -1,13 +1,15 @@
 // This is a very simple test, and could be made more robust.
 // All we do is check for the presence of "http://" or "https://"
-// at the start of the string.
+// at the start of the string. We ignore mailto: and tel: links
 
 const description = 'Relative links can break (i.e. if recipients are outside the company network) and make your content unavailable to view';
 
 
 export default (props) => {
   if (props.hasOwnProperty('href')) {
-    if (!/^(http|https):\/\//.test(props['href'])) {
+    if (!/^(http|https):\/\//.test(props['href']) &&
+        !/^mailto:/.test(props['href']) &&
+        !/^tel:/.test(props['href'])) {
       return new Error(description);
     }
   }
