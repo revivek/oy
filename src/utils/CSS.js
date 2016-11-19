228df4eaa3a83df0d6b30b5e9d536f4687a254fc
Vivek Patel <vivek@oysterbooks.com>
2016-11-19 17:48:16 -0500

    fix: make CleanCSS shorthanding false; fixes #62

--- src/utils/CSS.js
+++ src/utils/CSS.js
@@ -4,7 +4,9 @@ import CleanCSS from 'clean-css';
 export default {
   // Validate against common CSS vulnerabilities.
   raiseOnUnsafeCSS: (css = '', foundInName = '\'not provided\'') => {
-    css = new CleanCSS().minify(css).styles;
+    css = new CleanCSS({
+      shorthandCompacting: false
+    }).minify(css).styles;
     if (css.match(/-moz-binding/i)) {
       throw new Error(`Unsafe CSS found in ${foundInName}: "-moz-binding"`);
     } else if (css.match(/expression/i)) {
